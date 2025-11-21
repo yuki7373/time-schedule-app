eca562b8aedd927143e38d8cc5fa0433ecf4fcb1
yuki7373 <yuki73730904@gmail.com>
2025-11-21 17:53:03 +0900

    Update event listener syntax in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,22 @@
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open('schedule-cache-v1').then(cache => {
-            return cache.addAll([
-                './',
-                './index.html',
-                './style.css',
-                './app.js',
-                './manifest.json',
-                './icon.png'
-            ]);
-        })
-    );
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("schedule-cache-v1").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./app.js",
+        "./manifest.json",
+        "./icon.png"
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(res => {
-            return res || fetch(event.request);
-        })
-    );
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
